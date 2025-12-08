0a9248c420c5314f7d6b41917eefefba949a0aa0
alexasadullin <asadulinsd@gmail.com>
2025-12-08 17:57:46 +0300

    правки по итогам контеста: изменены binary_search и exponential_search в классе Solution, отредактирована документация

--- algorithms.py
+++ algorithms.py
@@ -1,26 +1,6 @@
 import time
 import tracemalloc
-
-
-def exponential_search(arr, target):
-    if len(arr) == 0:
-        return False
-    if arr[0] == target:
-        return True
-    i = 1
-    while i < len(arr) and arr[i] <= target:
-        i *= 2
-    left = i // 2
-    right = min(i, len(arr) - 1)
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == target:
-            return True
-        elif arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return False
+from bisect import bisect_left
 
 
 class Solution:
@@ -39,7 +19,7 @@ class Solution:
                 peak_memory = tracemalloc.get_traced_memory()[1]
                 tracemalloc.stop()
                 memory_used = max(peak_memory - start_memory, 1)  # Минимум 1 байт для логарифмической шкалы
-                return True, time.perf_counter() - start_time, memory_used
+                return self.arr_m[i], time.perf_counter() - start_time, memory_used
             elif self.arr_m[i] < self.arr_n[j]:
                 i += 1
             else:
@@ -48,48 +28,115 @@ class Solution:
         peak_memory = tracemalloc.get_traced_memory()[1]
         tracemalloc.stop()
         memory_used = max(peak_memory - start_memory, 1)  # Минимум 1 байт для логарифмической шкалы
-        return False, time.perf_counter() - start_time, memory_used
+        return -1, time.perf_counter() - start_time, memory_used
 
     def binary_search(self):
         tracemalloc.start()
         start_memory = tracemalloc.get_traced_memory()[1]
         start_time = time.perf_counter()
 
-        for elem in self.arr_m:
-            left, right = 0, len(self.arr_n) - 1
-            while left <= right:
-                mid = (left + right) // 2
-                if self.arr_n[mid] == elem:
-                    peak_memory = tracemalloc.get_traced_memory()[1]
-                    tracemalloc.stop()
-                    memory_used = max(peak_memory - start_memory, 1)  # Минимум 1 байт для логарифмической шкалы
-                    return True, time.perf_counter() - start_time, memory_used
-                elif self.arr_n[mid] < elem:
-                    left = mid + 1
-                else:
-                    right = mid - 1
+        if not self.arr_m or not self.arr_n:
+            peak_memory = tracemalloc.get_traced_memory()[1]
+            tracemalloc.stop()
+            memory_used = max(peak_memory - start_memory, 1)  # Минимум 1 байт для логарифмической шкалы
+            return -1, time.perf_counter() - start_time, memory_used
+        if len(self.arr_m) > len(self.arr_n):
+            self.arr_m, self.arr_n = self.arr_n, self.arr_m
+
+        result = next(
+            (elem for elem in self.arr_m
+             if (idx := bisect_left(self.arr_n, elem)) < len(self.arr_n) and self.arr_n[idx] == elem), # невный цикл while для ускорения (не проходит по органичениям контеста)
+            -1
+        )
 
         peak_memory = tracemalloc.get_traced_memory()[1]
         tracemalloc.stop()
         memory_used = max(peak_memory - start_memory, 1)  # Минимум 1 байт для логарифмической шкалы
-        return False, time.perf_counter() - start_time, memory_used
+        return result, time.perf_counter() - start_time, memory_used
 
     def exponential_search(self):
         tracemalloc.start()
         start_memory = tracemalloc.get_traced_memory()[1]
         start_time = time.perf_counter()
 
-        for elem in self.arr_m:
-            if exponential_search(self.arr_n, elem):
-                peak_memory = tracemalloc.get_traced_memory()[1]
-                tracemalloc.stop()
-                memory_used = max(peak_memory - start_memory, 1)  # Минимум 1 байт для логарифмической шкалы
-                return True, time.perf_counter() - start_time, memory_used
+        if not self.arr_m or not self.arr_n:
+            peak_memory = tracemalloc.get_traced_memory()[1]
+            tracemalloc.stop()
+            memory_used = max(peak_memory - start_memory, 1)
+            return -1, time.perf_counter() - start_time, memory_used
+        lo = max(self.arr_m[0], self.arr_n[0])
+        hi = min(self.arr_m[-1], self.arr_n[-1])
+        if lo > hi:
+            peak_memory = tracemalloc.get_traced_memory()[1]
+            tracemalloc.stop()
+            memory_used = max(peak_memory - start_memory, 1)
+            return -1, time.perf_counter() - start_time, memory_used
 
+        # нижняя граница в self.arr_m
+        l = 0
+        r = len(self.arr_m) - 1
+        i1 = 0
+        while l <= r:
+            m = (l + r) // 2
+            if self.arr_m[m] < lo:
+                i1 = m + 1
+                l = m + 1
+            else:
+                r = m - 1
+
+        # верхняя граница в self.arr_m
+        l = 0
+        r = len(self.arr_m) - 1
+        i2 = len(self.arr_m)
+        while l <= r:
+            m = (l + r) // 2
+            if self.arr_m[m] <= hi:
+                l = m + 1
+            else:
+                i2 = m
+                r = m - 1
+
+        # нижняя граница в self.arr_n
+        l = 0
+        r = len(self.arr_n) - 1
+        j1 = 0
+        while l <= r:
+            m = (l + r) // 2
+            if self.arr_n[m] < lo:
+                j1 = m + 1
+                l = m + 1
+            else:
+                r = m - 1
+
+        # верхняя граница в self.arr_n
+        l = 0
+        r = len(self.arr_n) - 1
+        j2 = len(self.arr_n)
+        while l <= r:
+            m = (l + r) // 2
+            if self.arr_n[m] <= hi:
+                l = m + 1
+            else:
+                j2 = m
+                r = m - 1
+
+        found = -1
+        ia = i1
+        ja = j1
+        while ia < i2 and ja < j2:
+            x = self.arr_m[ia]
+            y = self.arr_n[ja]
+            if x == y:
+                found = x
+                break
+            elif x < y:
+                ia += 1
+            else:
+                ja += 1
         peak_memory = tracemalloc.get_traced_memory()[1]
         tracemalloc.stop()
         memory_used = max(peak_memory - start_memory, 1)  # Минимум 1 байт для логарифмической шкалы
-        return False, time.perf_counter() - start_time, memory_used
+        return found, time.perf_counter() - start_time, memory_used
 
     def binary_divide(self):
         tracemalloc.start()
