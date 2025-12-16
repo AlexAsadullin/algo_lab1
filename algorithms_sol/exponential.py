efac615c3883823f0e3dc477aebda47f9baf0209
alexasadullin <asadulinsd@gmail.com>
2025-12-16 22:53:26 +0300

    fix exp search (delete useless ifs, added recursion back)

--- algorithms_sol/exponential.py
+++ algorithms_sol/exponential.py
@@ -1,48 +1,41 @@
-def exponential_search(arr_m, arr_n):
+def recursion(arr: list, start_index: int, target: int) -> int:
+    if start_index >= len(arr):
+        return -1
+    idx = 1
+    while start_index + idx < len(arr) and arr[start_index + idx] < target:
+        idx *= 2
+
+    l = start_index + (idx // 2)
+    r = min(start_index + idx, len(arr) - 1)
+
+    while l <= r:
+        m = (l + r) // 2
+        if arr[m] == target:
+            return m
+        elif arr[m] < target:
+            l = m + 1
+        else:
+            r = m - 1
+    return l
+
+
+def exponential_search(arr_m: list, arr_n: list):
     if len(arr_m) == 0 or len(arr_n) == 0:
         return -1
 
     if len(arr_m) > len(arr_n):
         arr_m, arr_n = arr_n, arr_m
-
-    M = len(arr_m)
+    #M = len(arr_m)
     N = len(arr_n)
+
     last_found_index = 0
+    for i in arr_m:
+        found_idnex = recursion(arr_n, last_found_index, i)
 
-    for i in range(M):
-        current_element = arr_m[i]
-
-        if last_found_index >= N or current_element > arr_n[-1]:
+        if found_idnex < N and arr_n[found_idnex] == i:
+            return True
+        last_found_index = found_idnex
+        if last_found_index >= N:
             break
 
-        if current_element < arr_n[last_found_index]:
-            low = 0
-            high = last_found_index - 1
-        else:
-            low = last_found_index
-            high = last_found_index
-            step = 1
-
-            while high < N and arr_n[high] < current_element:
-                low = high + 1
-                high = high + step
-                step *= 2
-
-            if high >= N:
-                high = N - 1
-
-        if low > high:
-            low, high = high, low
-
-        for j in range(low, high + 1):
-            if arr_n[j] == current_element:
-                return int(current_element)
-            elif arr_n[j] > current_element:
-                break
-
-        if high < N - 1 and arr_n[high] < current_element:
-            last_found_index = high + 1
-        else:
-            last_found_index = low
-
-    return -1
+    return False
